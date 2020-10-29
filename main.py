c738c35e3c83a0245b70e6de770982155132aff1
Steffen Arntz <steffen.arntz@ucware.de>
2020-10-29 10:59:09 +0100

    move to sqlite for future improvements, this still ain't nice :-(

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import imagehash
+import sqlite3
 from PIL import Image, ImageSequence
+import numpy as np
+import io
 
+
+def adapt_array(arr):
+    out = io.BytesIO()
+    np.save(out, arr)
+    out.seek(0)
+    return sqlite3.Binary(out.read())
+
+
+def convert_array(text):
+    out = io.BytesIO(text)
+    out.seek(0)
+    return np.load(out)
+
+
+sqlite3.register_adapter(np.ndarray, adapt_array)
+sqlite3.register_converter("array", convert_array)
+
+mem_db = sqlite3.connect("file::memory:?cache=shared", detect_types=sqlite3.PARSE_DECLTYPES)
 gif = Image.open('test.gif')
 gif_seq = ImageSequence.Iterator(gif)
+mem_db.execute('CREATE TABLE frame_data(id INTEGER PRIMARY KEY ASC, phash array, dhash array)')
+mem_db.execute(
+    'CREATE TABLE matches(id INTEGER PRIMARY KEY ASC, start INTEGER, end INTEGER, pdiff INTEGER, ddiff INTEGER, '
+    'FOREIGN KEY(start) REFERENCES frame_data(id), FOREIGN KEY(end) REFERENCES frame_data(id))')
+mem_de_cursor1 = mem_db.cursor()
+mem_de_cursor2 = mem_db.cursor()
 
 framecounter = 0
 for image in gif_seq:
@@ -11,34 +38,30 @@ for image in gif_seq:
 print('Sequence has ' + str(framecounter) + ' frames.')
 
 gif_seq = ImageSequence.Iterator(gif)
-image_hashes = []
-counter = 1
+counter = 0
 for image in gif_seq:
-    image_hashes.append(imagehash.phash(image, 24))
+    counter += 1
+    mem_de_cursor1.execute('INSERT INTO frame_data VALUES (?,?,?)',
+                           [counter, imagehash.phash(image, 24).hash, imagehash.dhash(image, 24).hash])
 
 counter1 = 0
 diffmap = []
-for hash1 in image_hashes:
+mem_de_cursor1.execute('SELECT * FROM frame_data')
+for (id1, phash1, dhash1) in mem_de_cursor1:
     counter2 = 0
     counter1 += 1
-    for hash2 in image_hashes:
+    mem_de_cursor2.execute('SELECT * FROM frame_data')
+    for (id2, phash2, dhash2) in mem_de_cursor2:
         counter2 += 1
-        difference = hash1 - hash2
-        diffmap.append([[counter1, counter2], difference])
+        if counter1 >= counter2:
+            continue
+        mem_db.execute("INSERT INTO matches VALUES (null, ?, ?, ?, ?)",
+                       [id1, id2, abs(imagehash.ImageHash(phash1) - imagehash.ImageHash(phash2)),
+                        abs(imagehash.ImageHash(dhash1) - imagehash.ImageHash(dhash2))])
 
-filtered_diffmap = []
-for frame in diffmap:
-    if frame[0][0] != frame[0][1] and frame[0][1] - frame[0][0] > 20 and frame[1] < 35:
-        filtered_diffmap.append(frame)
+mem_de_cursor1.execute(
+    "SELECT * FROM (SELECT * FROM matches WHERE (end - start) > 10  AND (ddiff < 100 OR pdiff < 100) ORDER BY ddiff "
+    "ASC, pdiff ASC LIMIT 25) ORDER BY ddiff DESC, pdiff DESC")
 
-del image
-del image_hashes
-del gif
-del gif_seq
-del diffmap
-filtered_diffmap.sort(key=lambda tup: tup[0][1] - tup[0][0])
-
-print("Start \t End \t Dur \t Diff")
-for frame in filtered_diffmap:
-    print(str(frame[0][0]).ljust(4), "\t", str(frame[0][1]).ljust(4), "\t", str(frame[0][1] - frame[0][0]), "\t",
-          str(frame[1]))
+for frame in mem_de_cursor1:
+    print(frame)
